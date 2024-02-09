6096ac1f886303aca2f484cdae9466b1acbf990a
blueteddy87 <41110746+blueteddy87@users.noreply.github.com>
2024-02-09 12:06:02 +0100

    firebase-b-up

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#36393B]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-red-800' >Work</p>
+          <p className='py-6'>check some of</p>
+        </div>
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          <div className='shadow-lg shadow-gray-900 group container rounded-md justify-center items-center mx-auto content-div'>
+            <div>
+              <span>
+
+              </span>
+              <div>
+                <a href="/">
+                  <button></button>
+                </a>
+                <a href="/">
+                  <button></button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
